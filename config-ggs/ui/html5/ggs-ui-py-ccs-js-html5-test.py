c646760a7651b709c9643f3d732c908c8f38f8d8
redrootmin <redroot@gmail.com>
2024-07-03 17:58:03 +0300

    update ui web testing

--- config-ggs/ui/html5/ggs-ui-py-ccs-js-html5-test.py
+++ config-ggs/ui/html5/ggs-ui-py-ccs-js-html5-test.py
@@ -9,7 +9,10 @@ import webview
 class Api():
   def log(self, value):
     print(value)
-    
-webview.create_window('GGS-web-gui-test', 'html5-ui-base-run-2.html', width=1064, height=512, js_api=Api())
-webview.start()
+    if value == "exit":
+       print("run exit")
+       window.destroy()    
+
+window = webview.create_window('GGS-web-gui-test', 'html5-ui-base-run-2.html', width=1064, height=512, js_api=Api())
+webview.start(window)
 
